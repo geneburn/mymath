ee788e85ab0de78adfc6e1fb2170a79727c7ae88
geneburn <geneburngen2@gmail.com>
2024-07-13 06:58:55 +0500

    some changes

--- mymath.go
+++ mymath.go
@@ -101,3 +101,9 @@ func Hypot(x, y float64) float64 {
 func Pow10(x int) float64 {
 	return math.Pow10(x)
 }
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
